3b0a1faa7ca3e6c2d9bcbd76ab0c83b326f0efe7
Eduardo Henrique Kenji Shibukawa <EduardoShibuka@gmail.com>
2018-02-02 19:22:13 -0200

    2.10. Tratamento correto das respostas HTTP 404 e 201

--- src/main/java/com/algaworks/socialbooks/resources/LivrosResources.java
+++ src/main/java/com/algaworks/socialbooks/resources/LivrosResources.java
@@ -1,14 +1,16 @@
 package com.algaworks.socialbooks.resources;
 
-import java.util.Arrays;
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.algaworks.socialbooks.domain.Livro;
 import com.algaworks.socialbooks.repository.LivrosRepository;
@@ -25,8 +27,15 @@ public class LivrosResources {
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
-	public void salvar(@RequestBody Livro livro) {
-		livrosrepository.save(livro);
+	public ResponseEntity<Void> salvar(@RequestBody Livro livro) {
+		livro = livrosrepository.save(livro);
+		
+		URI uri = ServletUriComponentsBuilder
+					.fromCurrentRequest()
+					.path("/{id}").buildAndExpand(livro.getId())
+					.toUri();
+		
+		return ResponseEntity.created(uri).build();
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
@@ -37,8 +46,14 @@ public class LivrosResources {
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public Livro buscar(@PathVariable("id") Long id) {
-		return livrosrepository.findOne(id);				
+	public ResponseEntity<Livro> buscar(@PathVariable("id") Long id) {
+		Livro livro =  livrosrepository.findOne(id);
+		
+		if (livro == null) {
+			return ResponseEntity.notFound().build(); 
+		}
+		
+		return ResponseEntity.ok(livro); 		
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
